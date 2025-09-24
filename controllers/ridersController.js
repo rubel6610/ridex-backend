b94594476736b9728a872399171829f88a8be844
Si Fad <sifayed00@gmail.com>
2025-09-25 03:30:26 +0600

    added riders get api

--- controllers/ridersController.js
+++ controllers/ridersController.js
@@ -1,5 +1,17 @@
 const { getCollection } = require('../utils/getCollection');
 
+// GET: Get all riders
+const getRiders = async (req, res) => {
+  try {
+    const ridersCollection = getCollection('riders');
+    const riders = await ridersCollection.find().toArray();
+    res.status(200).json({ riders });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error', error: error.message });
+  }
+};
+
 // POST: Simple rider insert (no checks)
 const addRiderDirectly = async (req, res) => {
   try {
@@ -21,4 +33,4 @@ const addRiderDirectly = async (req, res) => {
   }
 };
 
-module.exports = { addRiderDirectly };
+module.exports = { getRiders, addRiderDirectly };
--- routes/riderRoutes.js
+++ routes/riderRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const {
+  getRiders,
   addRiderDirectly,
 } = require('../controllers/ridersController');
 const router = express.Router();
 
+router.get('/riders', getRiders);
 router.post('/add-rider', addRiderDirectly);
 
 module.exports = router;
