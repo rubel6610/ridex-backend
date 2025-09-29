219e0e3fc6b5886c98d03d681854703999c6564d
MD RUBEL HOSEN <rubelhosen1310@gmail.com>
2025-09-30 02:50:19 +0600

    added socket.io

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 // index.js
+require('dotenv').config({ quiet: true });
 const express = require('express');
 const cors = require('cors');
 const { connectDB } = require('./config/db');
-require('dotenv').config({ quiet: true });
+const http = require('http');
+
 
 const userRoutes = require('./routes/userRoutes');
 const authRoutes = require('./routes/authRoutes'); 
 const riderRoutes = require('./routes/riderRoutes');
 const userManageRoutes = require('./routes/userManageRoutes');
 const riderManageRoutes = require('./routes/riderManageRoutes');
+const chatRoutes = require("./routes/chatRoutes")
+
+const { initSocket } = require('./socket/socket');
 
 const app = express();
+const server = http.createServer(app)
+
 
 // Middlewares
-app.use(cors({
-  origin: ['http://localhost:3000','http://localhost:3001','http://localhost:3001','https://ridex-fronted.netlify.app'],
-  credentials: true, 
-}));
+app.use(cors())
 app.use(express.json());
-
+initSocket(server)
 // Default route
 app.get("/", (req, res) => {
   res.send('🚀 Server is running...');
@@ -30,6 +34,7 @@ app.use('/api', userRoutes);
 app.use('/api', riderRoutes); 
 app.use('/api', userManageRoutes); 
 app.use('/api', riderManageRoutes); 
+app.use("/api/chat", chatRoutes)
 
 // Start server
 const PORT = process.env.PORT || 5000;
@@ -37,7 +42,7 @@ const PORT = process.env.PORT || 5000;
 (async () => {
   try {
     await connectDB(process.env.MONGO_URI);
-    app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+    server.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
   } catch (err) {
     console.error('❌ Failed to connect DB:', err);
     process.exit(1);
