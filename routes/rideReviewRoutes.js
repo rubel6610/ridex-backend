d08f4e8f0f96a6a34299299aa56f3b387940bda9
omarfaruk-dev <omarfaruk.dev@gmail.com>
2025-10-15 16:46:49 +0600

    get and post route added to rideReviewRoutes

--- routes/rideReviewRoutes.js
+++ routes/rideReviewRoutes.js
@@ -9,6 +9,10 @@ const {
 // POST => create a review
 router.post('/', createRideReview);
 
+// GET => all reviews for a specific rider
+router.get('/rider/:riderId', getRiderReviews);
 
+// GET => all ride reviews (for admin)
+router.get('/', getAllRideReviews);
 
 module.exports = router;
